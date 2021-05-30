92313bafd1e61e31c821cbd4613a519754c714f1
issueyh <issueyh12921@gmail.com>
2021-05-30 22:19:39 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-// 載入 express 並建構應用程式伺服器
+// 載入 express 
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose') // 載入 mongoose
+const app = express() //建構應用程式伺服器
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB，過渡期時 MongoDB 透過 DeprecationWarning 發出警告，需調整程式
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+    console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+    console.log('mongodb connected!')
+})
 
 // 設定首頁路由
 app.get('/', (req, res) => {
