f5cb566df48a272b06130658114e09377b01e23e
issueyh <issueyh12921@gmail.com>
2021-05-31 14:52:28 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars')
+const Todo = require('./models/todo') // 載入 Todo model
+
 const app = express() //建構應用程式伺服器
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB，過渡期時 MongoDB 透過 DeprecationWarning 發出警告，需調整程式
 
@@ -21,7 +23,10 @@ app.set('view engine', 'hbs')
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-    res.render('index')
+    Todo.find() // 取出 Todo model 裡的所有資料
+        .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+        .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+        .catch(error => console.error(error)) // 錯誤處理
 })
 
 // 設定 port 3000
