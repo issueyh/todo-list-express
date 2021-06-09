a34c610ff912d90446eb8964705756c1310df801
issueyh <issueyh12921@gmail.com>
2021-06-09 10:36:14 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
 // 載入 express 
 const express = require('express')
-const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars')
-const Todo = require('./models/todo') // 載入 Todo model
 const bodyParser = require('body-parser') // 引用 body-parser
 const methodOverride = require('method-override')
 const routes = require('./routes') // 引用路由器
+require('./config/mongoose')
 
 const app = express() //建構應用程式伺服器
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB，過渡期時 MongoDB 透過 DeprecationWarning 發出警告，需調整程式
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-    console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-    console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -27,7 +14,6 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true })) // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(methodOverride('_method')) // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(routes) // 將 request 導入路由器
-
 
 
 
